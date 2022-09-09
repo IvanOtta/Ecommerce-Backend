14f6379098537e0e5e1cdfb8abef5c97aafc848a
Ivan Otta <Ivanotta18@gmail.com>
2022-09-09 19:18:54 -0300

    primera entrega actualizada

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const express = require("express");
 const app = express();
 const { Router } = express;
 const router = Router();
+const cartRouter = Router();
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+
 const PORT = process.env.port || 8080;
 
 const server = app.listen(PORT, () => {
@@ -15,93 +20,148 @@ app.use(express.json());
 app.use(urlencoded({ extended: true }));
 app.use("/public", express.static(__dirname + "/public"));
 app.use("/api/productos", router);
+app.use("/api/carrito", cartRouter);
 
-const productsArg = [
-  {
-    id: 1,
-    title: "Camiseta Argentina Qatar 2022",
-    price: 18000,
-    thumbnail:
-      "http://localhost:8080/public/camiseta-adidas-argentina-2022-3.jpg",
-  },
-  {
-    id: 2,
-    title: "Pelota Qatar 2022",
-    price: 45000,
-    thumbnail:
-      "http://localhost:8080/public/pelota-qatar-2022-adidas-al-rihla-league-box-replica-numero-5-blanca-100040h57782001-1.jpg",
-  },
-  {
-    id: 3,
-    title: "Botines adidas Lionel Messi x Speedflow",
-    price: 19000,
-    thumbnail:
-      "http://localhost:8080/public/messi-botines-2022-adidas-x-speedflow-mi-historia-nm.jpg",
-  },
-];
+const Contenedor = require("./contenedor.js");
+
+const productos = new Contenedor("productos.txt");
+const carro = new Contenedor("carrito.txt");
 
 const admin = true;
 
-router.get("/:id", (req, res, next) => {
-  const { id } = req.params;
-  console.log(id);
-  const prodFound = productsArg.find((item) => item.id == id);
-  if (id) {
-    res.json(prodFound);
-  }else{
-    next()
-  }
+//PRODUCTOS
 
-})
+// app.all("*", (req, res) => {
+//   res.json({
+//     error: -2,
+//     description: `ruta ${req.url} metodo ${req.method} no autorizado`,
+//   });
+// });
 
-router.get('/', (req,res) => {
-    res.json(productsArg)
-})
-
-router.post('/admin', (req, res,next) => {
-    const {body} = req
-
-    const lastId = productsArg.length  
-    const newId = lastId + 1
-    body.price = parseInt(body.price)
-    body.id = parseInt(newId)
-    productsArg.push(body)
-
-    res.json({status: '200', new: body})
-    if(!admin){
-        next()
-    }
-})
-
-router.put('/:id', (req, res, next) => {
-    const {id} = req.params
-    const {body} = req
-
-    const prodUpdate = productsArg.find(item => item.id == id)
-    prodUpdate.price = body.price
-
-    res.json({status: '200', new: prodUpdate})
-    if(!admin){
-        next()
-    }
-})
-
-router.delete('/:id', (req, res, next) => {
-    const {id} = req.params
-    console.log(id)
-    if(productsArg.some(item => item.id == id)){
-        let obj = productsArg.find(item => item.id == id)
-        let posicion = productsArg.indexOf(obj)
-        productsArg = productsArg.splice(posicion,1)
-        res.json(productsArg)
-    }
-    res.json({status:'ok'})
-    if(!admin){
-        next()
-    }
-})
-
-
-app.all("/*", (req, res) => {
-  res.status(404).json({ error: "pagina no encontrada" });
+app.get("/form", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
+
+router.get("/", async (req, res) => {
+  const productsList = await productos.getAll();
+  res.send(await productsList);
+});
+
+router.post(
+  "/",
+  (req, res, next) => {
+    if (!admin) {
+      res.send({
+        error: -1,
+        descripcion: "no autorizado",
+      });
+    } else {
+      next();
+    }
+  },
+  urlencodedParser,
+  jsonParser,
+  async (req, res) => {
+    const { body } = req;
+
+    const newId = await productos.save(body);
+    res.json({ success: "ok", new: { ...body, id: newId } });
+  }
+)
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const productoId = await productos.getById(Number(id));
+  res.json(productoId);
+});
+
+router.put("/:id",(req, res, next) => {
+    if (!admin) {
+      res.send({ error: -1, descripcion: "no autorizado" });
+    } else {
+      next();
+    }},urlencodedParser,jsonParser,async (req, res) => {
+    const { body } = req;
+    console.log(body);
+    const id = Number(req.params.id);
+
+    const changesInProd = await productos.updateProduct(id, body);
+    res.json(await changesInProd);
+  }
+);
+
+router.delete(
+  "/:id",
+  (req, res, next) => {
+    if (!admin) {
+      res.send({
+        error: -1,
+        descripcion: "no autorizado",
+      });
+    } else {
+      next();
+    }
+  },
+
+  async (req, res) => {
+    const productoEliminado = await productos.deleteById(Number(req.params.id));
+    res.json(productoEliminado);
+  }
+);
+
+//              CARRITO DE COMPRA
+
+cartRouter.post("/", async (req, res) => {
+  let productos = [];
+  let timeStamp = Date.now();
+  const nuevoCarrito = await carro.save({ timeStamp, productos });
+  res.json(nuevoCarrito);
+});
+
+cartRouter.delete('/:id',(req, res, next) => {
+  if (!admin) {
+    res.send({
+      error: -1,
+      descripcion: "no autorizado",
+    });
+  } else {
+    next();
+  }
+} , async (req,res)=>{
+
+  const id = req.params.id;
+  let carritoEliminado = await carro.deleteById(Number(id));
+  res.json(carritoEliminado)
+})
+
+cartRouter.post('/:id/productos', urlencodedParser, jsonParser,  async (req,res)=>{
+  const id = req.params.id;
+  const { body } = req
+
+  const productoAgregado = await productos.getById(Number(body.id))
+  
+  const carritoID = await carro.getById(parseInt(id));
+  const productosCarrito = carritoID.productos;
+  productosCarrito.push(productoAgregado);
+  await carro.updateProduct(Number(id),{
+      productos: productosCarrito
+  })
+res.json(productosCarrito)
+
+})
+
+cartRouter.delete('/:id/productos/:id_prod', async (req,res)=>{
+  const id = req.params.id;
+  const id_prod = req.params.id_prod;
+
+  const carritoID = await carro.getById(Number(id))
+  const productosCarrito = carritoID.productos;
+ 
+  const prodDelete =  productosCarrito.filter(e => e.id !== Number(id_prod))
+
+  await carro.updateProduct(Number(id),{
+    productos: prodDelete
+})
+  res.json('done')
+
+})
